ca40f7868e4795534d2e41bee2017bf097d326f0
ffxx68 <ffumi68@gmail.com>
2025-03-18 12:17:40 +0100

    introduced 'keycode' but not fully tested yet

--- Demos/io_libs/main.c
+++ Demos/io_libs/main.c
@@ -13,9 +13,10 @@ char xram cBlank[2] = " ";
 char xram cNumber[6];
 char xram cPrint[24] = "Hello world!";
 
-#include key.h  // PC-1403 specific
-#include puts.h  // PC-1403 specific
-#include getkey.h // PC-1403 specific
+#include keycode.h  // PC-1403 specific
+#include getkey.h   // PC-1403 specific
+#include puts.h     // PC-1403 specific
+
 #include int2str.h
 #include strcat.h
 
@@ -23,6 +24,7 @@ main()
 {
 	puts ( &cPrint );
 	bTmp1=getkey();
+	
 	/*
 	for (bIdx1 = 0; bIdx1 <= 3; bIdx1++) {
 		cPrint[0] = 0;
@@ -39,20 +41,27 @@ main()
 		bTmp1=getkey();
 	}
 	*/
-	#asm
-WAITLOOP:
-	LII 07
-	WAIT 255
-	DECI
-	JRNZM WAITLOOP
-	#endasm
+
+	// wait a bit, before going to the keycode()
+	for (bIdx1 = 0; bIdx1 <= 100; bIdx1++) {
+		#asm
+			LII 07
+		WAITLOOP:
+			WAIT 255
+			DECI
+			JRNZM WAITLOOP
+		#endasm
+	}
+	
 	wTmp = 0;
 	bIdx1 = 128;
+	
 	while (bIdx1 == 128)
 	{
 		bIdx1 = keycode();
 		wTmp++;
 	}
+	
 	cNumber[0] = 0;
 	cPrint[0] = 0;
 	int2str(bIdx1, &cNumber);
