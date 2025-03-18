5e7e7aca5563799e919fe6dc1e21192c25ebc27b
ffxx68 <ffumi68@gmail.com>
2025-03-18 12:15:57 +0100

    fix

--- getkey.h
+++ getkey.h
@@ -1,7 +1,15 @@
-// using routines from PC-1403 ROM
 char getkey () {
 #asm	
-    CALL  0x1494        ; Syscall: wait for a keystroke (PC-1403)
+    CALL  0x1494        ; Syscall: wait for a keystroke (from PC-1403 ROM)
+	LIA	LB(0xFF5E)	    ; LB of key-code address
+	LIB	HB(0xFF5E)	    ; HB of key-code address
+	LP 4
+	EXAM		        ; LB -> Xl
+	EXAB
+	INCP		
+	EXAM                ; HB -> Xh
+	DX
+	IXL                 ; set return value in A
 #endasm
-	return readbyte(0xFF5E); // get the key-code value stored in memory
+	return;
 }
