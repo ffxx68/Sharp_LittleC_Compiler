16ab598fbb83f109178cc7f18a89e992578bbd16
ffxx68 <ffumi68@gmail.com>
2026-04-14 13:44:26 +0200

    fixed byte array init

--- Source/lcc/CodeGen.pas
+++ Source/lcc/CodeGen.pas
@@ -830,7 +830,7 @@ begin
   end;
 
   addasm(nm + ':'#9'; Variable ' + nm + ' = (' + s + ')');
-  if (typ = 'char') or (typ = 'char') or (typ = 'float') then
+  if (typ = 'byte') or (typ = 'char') or (typ = 'float') then
     s := #9'.DB'#9 + s
   else if (typ = 'word') then
     s := #9'.DW'#9 + s;
@@ -982,7 +982,7 @@ end;
 {--------------------------------------------------------------}
 
 
-{---------------------------------------------------------------}
+{--------------------------------------------------------------}
 { Comparison Equal }
 
 procedure CompEqual;
