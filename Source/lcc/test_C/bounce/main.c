c998206e7cd8342308aa3cb4d5d1bb638937bb07
ffxx68 <ffumi68@gmail.com>
2026-04-14 13:46:32 +0200

    Snake bounce

--- Source/lcc/test_C/bounce/main.c
+++ Source/lcc/test_C/bounce/main.c
@@ -7,37 +7,38 @@
 #include lcd.h  // PC-1403 specific
 #include key.h  // PC-1403 specific
 
-byte xram bKey1, bKey2, bTmpX, bTmpY, bBrk;
+byte xram bKey1, bKey2, bTmpX, bTmpY, bBrk, bTailX, bTailY;
 byte xram bCpos, bCpos_old, bLoopNr;
-//byte xram bSnakePos;
-byte xram bPoints;
+byte xram bSnakePos;
+byte xram bPntX, bPntY, bPntMode;
 byte xram bDxBall, bDyBall;
 byte xram bRnd;
-//byte xram bXSnake[6] = (0,0,0,0,0,0);
-//byte xram bYSnake[6] = (0,0,0,0,0,0);
+byte xram bXSnake[4];
+byte xram bYSnake[4];
 
 main()
 {
 	bCpos = 50; // initial cursor position
 	bCpos_old = 50;
+	bDxBall = 1;
+	bDyBall = 0;
 
 	bKey1 = 0xFF; // no key
 	bKey2 = 0xFF; // no key
 	bBrk = 0;
 	
-	//bSnakePos = 0;
-	bTmpX = 50; 
-	bTmpY = 7;
-	//bYSnake[bSnakePos] = bTmpY;
-	//bXSnake[bSnakePos] = bTmpX;
+	bSnakePos = 0;
+	bTmpX = 50;
+	bTmpY = 6;
+	bPntX = 100;
+	bPntY = 6;
+	bPntMode = 1;
 	bLoopNr = 0;
-	
-	bPoints = 0;
 	
 	lcd_on();
 	lcd_cls(0);
 	lcd_hline(bCpos, 0, 5, 1);
-	
+
 	// main loop
 	while (bBrk == 0)
 	{
@@ -78,14 +79,14 @@ RND_SKIP:
 		// bouncing ball
 		if (bLoopNr == 10) { // delay
 		
-			// clear current position
-			//bTmpX = bXSnake[bSnakePos]; 
-			//bTmpY = bYSnake[bSnakePos];
-			lcd_pset(bTmpX, bTmpY, 0);
-			
+			// clear tail of snake (oldest position in circular buffer)
+			bTailX = bXSnake[bSnakePos];
+			bTailY = bYSnake[bSnakePos];
+			lcd_pset(bTailX, bTailY, 0);
+
 			// boundary check and direction update
 			if (bTmpX == 0) bDxBall=1;
-			if (bTmpX == 100) bDxBall=0;
+			if (bTmpX == 99) bDxBall=0;
 			if (bTmpY == 1) {
 				bDyBall=1;
 				
@@ -95,28 +96,33 @@ RND_SKIP:
 				
 				// check cursor hit and update points
 				if (bTmpX>=bCpos && bTmpX<(bCpos+5)) {
-					bPoints++;
-					if (bPoints > 10) { 
-						bPoints=1;
-						lcd_hline(101, 7, 10, 0); 
+					lcd_pset(bPntX, bPntY, bPntMode);
+					bPntX++;
+					if (bPntX > 119) {
+						bPntX=100;
+						bPntY--;
+                        if (bPntY == 0) {
+                            bPntY = 6;
+                            if (bPntMode==1) bPntMode=0; else bPntMode=1;
+                        }
 					}
-					lcd_pset(101+bPoints, 7, 1);
 				}
 			}
-			if (bTmpY == 7) bDyBall=0 ;
-			
+			if (bTmpY == 6) bDyBall=0 ;
+
 			// calc new position
 			if (bDxBall == 1) bTmpX=bTmpX+1 ; else bTmpX=bTmpX-1;
 			if (bDyBall == 1) bTmpY=bTmpY+1 ; else bTmpY=bTmpY-1;
 			
-			// draw new position
+			// draw new head
 			lcd_pset(bTmpX, bTmpY, 1);
-			//bXSnake[bSnakePos] = bTmpX; 
-			//bYSnake[bSnakePos] = bTmpY;
-			
+			// store new head in circular buffer, advance tail pointer
+			bXSnake[bSnakePos] = bTmpX;
+			bYSnake[bSnakePos] = bTmpY;
+
 			bLoopNr = 0;
-			//bSnakePos++;
-			//if (bSnakePos == 2) bSnakePos = 0;
+			bSnakePos++;
+			if (bSnakePos == 4) bSnakePos = 0;
 		}
 		
 		bLoopNr++;
